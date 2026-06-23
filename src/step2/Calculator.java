ccccf7579fd5afd38208a26f03ef781b20f1c44e
chaeb0414-collab <chaeb0414@gmail.com>
2026-06-23 11:57:25 +0900

    컬렉션 타입 필드를 가진 Calculator클래스 생성

--- src/step2/Calculator.java
+++ src/step2/Calculator.java
@@ -1,4 +1,25 @@
 package step2;
 
+import java.util.ArrayList;
+
 public class Calculator {
+
+    public List<Integer> results = new ArrayList<>();
+
+    public Integer calculate(int num1, int num2, char operator) {
+
+        int result;
+
+        switch (ch) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                result = num1 / num2;
+            }
+            default -> throw new IllegalArgumentException("지원하지 않는 연산자 입니다.");
+        }
+        return result;
+    }
 }
